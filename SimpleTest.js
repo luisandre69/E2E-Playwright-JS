fca3249ea542aae8f91b7ea9fa5dd47e2f784ead
Luis Abreu <luisandre_69@hotmail.com>
2021-03-30 16:39:02 +0100

    simple test

---
+++ SimpleTest.js
@@ -0,0 +1,12 @@
+const {chromium} = require('playwright');
+
+(async() => {
+
+    const browser = await chromium.launch({
+        headless:false
+    });
+    const page = await browser.newPage();
+    await page.goto("https://executeautomation.com");
+    await page.screenshot({ path: 'ea.png' });
+    await browser.close();
+})();
