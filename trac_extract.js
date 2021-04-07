975cbc8522be6403681331b965a6f84948cb7bc7
Luis Abreu <luisandre_69@hotmail.com>
2021-04-07 14:23:19 +0100

    trac

--- trac_extract.js
+++ trac_extract.js
@@ -1,9 +1,9 @@
-const { chromium } = require('playwright');
-const fs = require('fs');
+const { chromium } = require("playwright");
+const fs = require("fs");
 
 (async () => {
   const browser = await chromium.launch({
-    headless: false
+    headless: false,
   });
 
   const context = await browser.newContext();
@@ -11,53 +11,60 @@ const fs = require('fs');
   // Open new page
   const page = await context.newPage();
 
-   // Go to http://eaapp.somee.com/
-   await page.goto('http://eaapp.somee.com/');
+  // Go to http://eaapp.somee.com/
+  await page.goto("http://eaapp.somee.com/");
 
-   await browser.startTracing(page,{path:`trace.json`, screenshots:true, categories: ['devtools.timeline']});
-   // Click text="Login"
-   await page.click('text="Login"');
-   // assert.equal(page.url(), 'http://eaapp.somee.com/Account/Login');
+  await browser.startTracing(page, {
+    path: `trace.json`,
+    screenshots: true,
+    categories: ["devtools.timeline"],
+  });
+  // Click text="Login"
+  await page.click('text="Login"');
+  // assert.equal(page.url(), 'http://eaapp.somee.com/Account/Login');
 
- 
-   // Click input[name="UserName"]
-   await page.click('#UserName');
- 
-   // Fill input[name="UserName"]
-   await page.fill('#UserName:visible', 'admin');
- 
-   // Click input[name="Password"]
-   await page.click('input[name="Password"]');
- 
-   // Fill input[name="Password"]
-   await page.fill('input[name="Password"]', 'password');
+  // Click input[name="UserName"]
+  await page.click("#UserName");
 
-   await browser.stopTracing();
- 
-   // Click input[type="submit"]
-   await page.click("input:text('Log in')");
-   // assert.equal(page.url(), 'http://eaapp.somee.com/');
- 
-   // Click text="Employee List"
-   await page.click('text="Employee List"');
+  // Fill input[name="UserName"]
+  await page.fill("#UserName:visible", "admin");
 
+  // Click input[name="Password"]
+  await page.click('input[name="Password"]');
 
-   const traceInfo = JSON.parse(fs.readFileSync('./trace.json', 'utf8'));
-   const traceData = traceInfo.traceEvents.filter(x => (
-    x.cat === 'disabled-by-default-devtools.screenshot' &&
-    x.name === 'Screenshot' &&
-    typeof x.args !== 'undefined' &&
-    typeof x.args.snapshot !== 'undefined'
-   ));
+  // Fill input[name="Password"]
+  await page.fill('input[name="Password"]', "password");
 
-   traceData.forEach(function(snap, index) {
-     fs.writeFile(`trace-screenshot-${index}.png`, snap.args.snapshot, 'base64', function(err) {
-      if (err) {
-        console.log('write error', err);
+  await browser.stopTracing();
+
+  // Click input[type="submit"]
+  await page.click("input:text('Log in')");
+  // assert.equal(page.url(), 'http://eaapp.somee.com/');
+
+  // Click text="Employee List"
+  await page.click('text="Employee List"');
+
+  const traceInfo = JSON.parse(fs.readFileSync("./trace.json", "utf8"));
+  const traceData = traceInfo.traceEvents.filter(
+    (x) =>
+      x.cat === "disabled-by-default-devtools.screenshot" &&
+      x.name === "Screenshot" &&
+      typeof x.args !== "undefined" &&
+      typeof x.args.snapshot !== "undefined"
+  );
+
+  traceData.forEach(function (snap, index) {
+    fs.writeFile(
+      `trace-screenshot-${index}.png`,
+      snap.args.snapshot,
+      "base64",
+      function (err) {
+        if (err) {
+          console.log("write error", err);
+        }
       }
-     });
-   })
- 
-   await browser.close();
- 
- })();
+    );
+  });
+
+  await browser.close();
+})();
